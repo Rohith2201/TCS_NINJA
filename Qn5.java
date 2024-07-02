85b42951ddc0d8d93478a0e5844769907fdaf449
J VENKATA LAKSHMI SAI ROHITH <94969985+Rohith2201@users.noreply.github.com>
2024-07-02 12:06:11 +0530

    Create Qn5.java

---
+++ Qn5.java
@@ -0,0 +1,56 @@
+Alice had to go to play with his friends. But his brother is not leaving. So, he thought to ask a question so that in the mean time he can go. 
+So , the problem is as follows: He will be given numbers n,m and k. Calculate T. (T = (nm)/k) His brother has to find the three coordinates of the XY plane (2D points) 
+such that the area of the triangle formed by those points should be equal to T. If there is any solution print YES. else NO.
+
+NOTE: 1≤ x1,y1, x2,y2, x3, y3 ≤ 109
+
+Example 1:
+Input:
+4 3 3
+
+Output:
+YES
+
+Explanation:
+One possible way is (1,0), (2,3) and (4,1) are the
+points where there area is equal to T.
+
+
+
+Example - 2:
+Input:
+4 4 7
+
+Output.
+NO
+
+Constraints:
+1 ≤ n,m≤ 109
+2 ≤ k ≤ 109
+
+The input format for testing:
+•The first line represents the n, m and k, each separated by a space.
+The Output format for testing:
+•Print YES if there is any solution else NO
+
+Instructions:
+•The system does not allow any kind of hard- coded input value/ values.
+•Written program code by the candidate will be verified against the inputs which are supplied from the system.
+
+Test Cases:
+Case 1
+Input (stdin)
+4  3  3
+
+Output (stdout)
+YES
+
+Case 2
+Input (stdin)
+2 5 8
+
+Output (stdout)
+NO
+
+CODE(JAVA):
+
