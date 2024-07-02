88ad1a683c42b8c567493f8a3e44419784d04dc4
J VENKATA LAKSHMI SAI ROHITH <94969985+Rohith2201@users.noreply.github.com>
2024-07-02 12:57:57 +0530

    Create Qn12.java

---
+++ Qn12.java
@@ -0,0 +1,35 @@
+Given N Rupees. A liter plastic bottle of milk costs R1 Rupees and a liter of the glass bottle of milk costs R2 Rupees. 
+But the empty glass bottle after buying can be exchanged for R3 Rupees. Find the maximum liters of milk which can be bought with N Rupees.
+
+Example-1:
+Input:
+10 →Value of N
+11 →Value of R1 i.e. price of plastic bottle
+9→Value of R2 i.e. price of glass bottle
+8 →Value of R3 i.e. price of empty glass bottle
+
+Output:
+2
+
+Test Cases:
+Case 1
+Input (stdin)
+10
+11
+9
+8
+
+Output (stdout)
+2
+
+Case 2
+Input (stdin)
+20
+15
+10
+5
+
+Output (stdout)
+3
+
+CODE(JAVA):
